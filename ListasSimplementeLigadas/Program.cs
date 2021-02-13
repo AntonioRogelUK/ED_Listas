04baf720eaac626589d551cd70bf96f46919dfb1
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2021-02-13 11:58:14 -0700

    Agregar archivos de proyecto.

---
+++ ListasSimplementeLigadas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListasSimplementeLigadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
