1dc361601f5e19a6222839f15bd19fedfb104357
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2021-02-13 12:56:36 -0700

    Agregando la clase Nodo

--- ListasSimplementeLigadas/Program.cs
+++ ListasSimplementeLigadas/Program.cs
@@ -6,7 +6,15 @@ namespace ListasSimplementeLigadas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Nodo nodo = new Nodo();
+
+            Console.WriteLine(nodo.Valor); //Esto utiliza el get
+            
+            nodo.Valor = "Un valor"; //Esto utiliza el set
+
+            //nodo.Valor = "ABC";
+
+            Console.WriteLine(nodo.Valor);
         }
     }
 }
