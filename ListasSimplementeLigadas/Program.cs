95a0fa89a491835d70b57d639e441342074f6fe6
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2021-02-13 14:48:55 -0700

    Clase lista

--- ListasSimplementeLigadas/Program.cs
+++ ListasSimplementeLigadas/Program.cs
@@ -6,15 +6,7 @@ namespace ListasSimplementeLigadas
     {
         static void Main(string[] args)
         {
-            Nodo nodo = new Nodo();
-
-            Console.WriteLine(nodo.Valor); //Esto utiliza el get
-            
-            nodo.Valor = "Un valor"; //Esto utiliza el set
-
-            //nodo.Valor = "ABC";
-
-            Console.WriteLine(nodo.Valor);
+           
         }
     }
 }
